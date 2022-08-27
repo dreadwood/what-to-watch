041401f7a0b594d916c4812ea8f20401c2d78abf
srg <skabassjob@gmail.com>
2022-08-27 16:32:04 +0400

    2.1 create main page component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,21 @@
-function App(): JSX.Element {
-  return <p>Guten tag, Welt!</p>;
+import MainPage from '../main-page/main-page';
+
+type AppProps = {
+  promoFilmData: {
+    NAME: string,
+    GENRE: string,
+    DATE: number,
+  }
+}
+
+function App({promoFilmData}: AppProps): JSX.Element {
+  return (
+    <MainPage
+      filmName={promoFilmData.NAME}
+      filmGenre={promoFilmData.GENRE}
+      filmDate={promoFilmData.DATE}
+    />
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const PromoFilmData = {
+  NAME: 'The Grand Budapest Hotel',
+  GENRE: 'Drama',
+  DATE: 2014,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      promoFilmData={PromoFilmData}
+    />
   </React.StrictMode>,
 );
