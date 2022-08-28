0d0367e18b23264867eb15301104ff7f22d6207f
srg <skabassjob@gmail.com>
2022-08-28 23:03:36 +0400

    4.1 add mocks

---
+++ src/types/films.ts
@@ -0,0 +1,19 @@
+export type Film = {
+  id: string
+  name: string
+  poster: string
+  smallPoster: string
+  backgroundPoster: string
+  colorPoster: string
+  video: string
+  previewVideo: string
+  director: string
+  cast: string[]
+  released: number
+  duration: number
+  genre: string
+  description: string
+  rating: number
+  reviewsount: number
+  isFavorite: boolean
+}
