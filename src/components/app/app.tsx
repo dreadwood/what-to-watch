02c6108d69a58f3c41dce5c06bfb4376fec1c95e
srg <skabassjob@gmail.com>
2022-08-28 10:54:30 +0400

    3.2 add routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import AddReviewPage from '../../pages/add-review-page/add-review-page';
+import ErrorPage from '../../pages/error-page/error-page';
+import FilmPage from '../../pages/film-page/film-page';
 import MainPage from '../../pages/main-page/main-page';
+import MyListPage from '../../pages/my-list-page/my-list-page';
+import PlayerPage from '../../pages/player-page/player-page';
+import SignInPage from '../../pages/sign-in-page/sign-in-page';
 
 type AppProps = {
   promoFilmData: {
@@ -10,11 +18,45 @@ type AppProps = {
 
 function App({promoFilmData}: AppProps): JSX.Element {
   return (
-    <MainPage
-      filmName={promoFilmData.NAME}
-      filmGenre={promoFilmData.GENRE}
-      filmDate={promoFilmData.DATE}
-    />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={
+            <MainPage
+              filmName={promoFilmData.NAME}
+              filmGenre={promoFilmData.GENRE}
+              filmDate={promoFilmData.DATE}
+            />
+          }
+        />
+        <Route
+          path={AppRoute.SignIn}
+          element={<SignInPage />}
+        />
+        <Route
+          path={AppRoute.MyList}
+          element={<MyListPage />}
+        />
+        <Route
+          path={AppRoute.Film}
+          element={<FilmPage />}
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={<AddReviewPage />}
+        />
+        <Route
+          path={AppRoute.Player}
+          element={<PlayerPage />}
+        />
+        <Route
+          path={AppRoute.NotFound}
+          element={<ErrorPage />}
+        />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
