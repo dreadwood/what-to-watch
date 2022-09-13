ae88d23d179f0dd71ec6ac2c948be22220b35a48
srg <skabassjob@gmail.com>
2022-09-13 23:22:38 +0400

    7.3 crate action for load films

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,5 +1,7 @@
 import {createAction} from '@reduxjs/toolkit';
+import {Film} from '../types/film';
 
 export const changeGenre = createAction<string>('filmList/changeGenre');
 export const resetFilmList = createAction('filmList/reset');
 export const addShownCards = createAction<number>('filmList/addShownCards');
+export const loadFilms = createAction<Film[]>('data/loadFilms');
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -2,17 +2,26 @@ import {createReducer} from '@reduxjs/toolkit';
 import {DEFAULT_GENRE, DEFAULT_QUANTITY_SHOWN_CARDS} from '../const';
 import {films} from '../mocks/films';
 import {Film} from '../types/film';
-import {changeGenre, addShownCards, resetFilmList} from './action';
+import {changeGenre, addShownCards, resetFilmList, loadFilms} from './action';
 
 const getListGenres = (allFilms: Film[]) => new Set(
   allFilms.reduce((acc, film) => [...acc, film.genre], [] as string[]),
 );
 
-const initialState = {
+type InitialState = {
+  activeGenre: string
+  genres: string[]
+  films: Film[]
+  quantityShownCards: number
+  isDataLoaded: boolean
+}
+
+const initialState: InitialState = {
   activeGenre: DEFAULT_GENRE,
   genres: [DEFAULT_GENRE, ...getListGenres(films)],
-  films,
+  films: [],
   quantityShownCards: DEFAULT_QUANTITY_SHOWN_CARDS,
+  isDataLoaded: false,
 };
 
 
@@ -23,6 +32,10 @@ export const reducer = createReducer(initialState, (builder) => {
     })
     .addCase(addShownCards, (state, action) => {
       state.quantityShownCards += action.payload;
+    })
+    .addCase(loadFilms, (state, action) => {
+      state.films = action.payload;
+      state.isDataLoaded = true;
     })
     .addCase(resetFilmList, () =>
       ({...initialState})
