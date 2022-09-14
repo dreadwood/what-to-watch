ae395d7fa38789f4e30a27a60d90237d29b07da2
srg <skabassjob@gmail.com>
2022-09-14 12:34:28 +0400

    7.8 create module token

--- src/const.ts
+++ src/const.ts
@@ -4,6 +4,7 @@ export const DEFAULT_QUANTITY_SHOWN_CARDS = 8;
 
 export const BACKEND_URL = 'https://9.react.pages.academy/wtw';
 export const REQUEST_API_TIMEOUT = 5000;
+export const AUTH_TOKEN_KEY_NAME = 'what-to-watch-token';
 
 export enum AppRoute {
   Root = '/',
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,21 @@
-import axios from 'axios';
+import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
 import {BACKEND_URL, REQUEST_API_TIMEOUT} from '../const';
+import {getToken} from './token';
 
-export const createApi = () => {
+export const createApi = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_API_TIMEOUT,
+  });
+
+  api.interceptors.request.use((config: AxiosRequestConfig) => {
+    const token = getToken();
+
+    if (token) {
+      config.headers['x-token'] = token;
+    }
+
+    return config;
   });
 
   return api;
