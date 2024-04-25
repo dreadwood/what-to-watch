524fd70a43a6cad6802c1eafcd20c7eac1ced5df
srg <skabassjob@gmail.com>
2024-04-25 23:48:00 +0400

    feat: update backend url

--- src/const.ts
+++ src/const.ts
@@ -3,7 +3,7 @@ export const STEP_SHOW_CARDS = 8;
 export const DEFAULT_QUANTITY_SHOWN_CARDS = 8;
 export const MAX_SIMILAR_FILMS = 4;
 
-export const BACKEND_URL = 'https://9.react.pages.academy/wtw';
+export const BACKEND_URL = 'https://11.react.htmlacademy.pro/wtw';
 export const REQUEST_API_TIMEOUT = 5000;
 export const AUTH_TOKEN_KEY_NAME = 'what-to-watch-token';
 
